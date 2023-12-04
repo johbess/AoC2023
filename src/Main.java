7fcc4d95c87fab91f15e84029c3b53868a39d0ad
johbess <johan.besseling@gmail.com>
2023-12-04 22:43:34 +0100

    dayone complete

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("./resources/dayOne/calibrationInput");
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+        ArrayList<Integer> calibrationValues = new ArrayList<>();
+
+        while ((line = br.readLine()) != null) {
+
+            boolean foundFirst = false;
+            boolean foundLast = false;
+
+
+            int start = 0;
+            int end = line.length() - 1;
+
+            Character first = null;
+            Character last = null;
+
+            while (!foundFirst || !foundLast) {
+
+                if (!foundFirst && start < line.length()) {
+                    char c = line.charAt(start);
+                    if (Character.isDigit(c)) {
+                        first = c;
+                        foundFirst = true;
+                    } else {
+                        start++;
+                    }
+                }
+
+                if (!foundLast && end >= 0) {
+                    char c = line.charAt(end);
+                    if (Character.isDigit(c)) {
+                        last = c;
+                        foundLast = true;
+                    } else {
+                        end--;
+                    }
+                }
+            }
+
+            int calibrationValue = Integer.parseInt(first + String.valueOf(last));
+            calibrationValues.add(calibrationValue);
+        }
+
+        System.out.println(calibrationValues.stream().reduce(0, Integer::sum));
+    }
+}
